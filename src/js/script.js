2263beabfa4595b4b0dca2db1a2615891a280090
Adesh Katiya <adeshkatiya27@gmail.com>
2025-05-31 18:14:43 +0530

    feat: change display image on clicking thumbnail image
    add slider animation for changing the image on thumbnail image click

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,38 @@
 const lightboxGallery = document.querySelector(".lightbox-gallery");
-const displayImage = document.querySelector(".lightbox-gallery__display-image");
+const displayImage = document.getElementById("main-display-image");
 const thumnailImages = lightboxGallery.querySelectorAll(".lightbox-gallery__thumbnail-image");
+const slider = document.getElementById("image-slider");
+
 const cartBtn = document.getElementById("cart-btn");
 const cartModal = document.getElementById("cart-modal");
+
+thumnailImages.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+        const newSrc = thumbnail.src.replace("-thumbnail", "");
+
+        const currentImage = slider.querySelector("img");
+
+        const newImage = currentImage.cloneNode(true);
+        newImage.src = newSrc;
+
+        slider.appendChild(newImage);
+
+        slider.style.transform = "translateX(-100%)";
+
+        setTimeout(() => {
+            slider.removeChild(currentImage);
+
+            slider.style.transition = "none";
+            slider.style.transform = "translateX(0)";
+
+            void slider.offsetWidth;
+
+            slider.style.transition = "transform 0.5s ease-in-out";
+        }, 500);
+    });
+});
+
+
 
 const lightboxOverlay = document.createElement("div");
 lightboxOverlay.classList.add("lightbox-gallery__overlay");
