dd77fce9f20ed686c6127cc32d5f0545f417247a
Adesh Katiya <adeshkatiya27@gmail.com>
2025-06-01 23:53:43 +0530

    feat: add increase/decrease quantity update and adding cart item and remove it

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ const displayImage = document.querySelector(".lightbox-gallery__display-image");
 const overlay = document.getElementById("lightbox-overlay");
 const lightboxOverlay = overlay.querySelector(".lightbox-gallery--overlay");
 const thumnailImages = lightboxGallery.querySelectorAll(".lightbox-gallery__thumbnail-image");
-console.log(thumnailImages);
 
 const overlayThumbnailImages = overlay.querySelectorAll(".lightbox-gallery__thumbnail-image");
 const imageSlider = document.getElementById("image-slider");
@@ -97,7 +96,6 @@ function slideToImage(index, direction) {
 }
 
 function updateActiveThumbnail(index) {
-    console.log(index);
     thumnailImages.forEach((thumb, i) => {
         thumb.classList.toggle("active", i === index);
     });
@@ -110,7 +108,6 @@ prevBtn.addEventListener("click", () => {
     if (currentIndex > 0) {
         currentIndex--;
         slideToImage(currentIndex, "prev");
-        // console.log(currentIndex);
         updateActiveThumbnail(currentIndex);
     }
 });
@@ -119,7 +116,6 @@ nextBtn.addEventListener("click", () => {
     if (currentIndex < imagePaths.length - 1) {
         currentIndex++;
         slideToImage(currentIndex, "next");
-        // console.log(currentIndex);
         updateActiveThumbnail(currentIndex);
     }
 });
@@ -132,7 +128,6 @@ nextBtn.addEventListener("click", () => {
 // lightboxGallery.appendChild(lightboxOverlay);
 
 overlay.addEventListener("click", (e) => {
-    console.log(e.target, e.currentTarget);
     if (e.target === overlay) {
         overlay.classList.remove("active");
     }
@@ -162,7 +157,6 @@ closeButton.addEventListener("click", () => {
     }, 500);
 
     updateActiveThumbnail(currentIndex);
-    console.log(currentIndex);
 });
 
 
@@ -188,7 +182,8 @@ closeButton.addEventListener("click", () => {
 //     lightboxOverlay.classList.remove("active");
 // });
 
-cartBtn.addEventListener("click", () => {
+cartBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
     cartModal.classList.toggle("active");
 });
 
@@ -197,3 +192,122 @@ window.addEventListener("DOMContentLoaded", () => {
         document.getElementById("cart-modal").classList.add("mounted");
     }, 50);
 });
+
+const decreaseBtn = document.getElementById("decrease");
+const increaseBtn = document.getElementById("increase");
+const inputQuantity = document.getElementById("quantity");
+const quantityForm = document.getElementById("quantity-form");
+const addToCartBtn = document.getElementById("add-to-cart");
+
+console.log(inputQuantity.value);
+
+decreaseBtn.addEventListener("click", () => {
+    if (inputQuantity.value > 0) {
+        inputQuantity.value--;
+    }
+    return;
+});
+
+increaseBtn.addEventListener("click", () => {
+    inputQuantity.value++;
+});
+
+const cartContents = document.getElementById("cart-contents");
+const cartItem = document.getElementById("cart-item");
+const emptyCartHTML = `
+        <p class="header__cart-contents" id="cart-contents">Your cart is empty.</p>
+    `;
+
+function addToCart(quantity) {
+    const existingItem = cartModal.querySelector(".header__cart-item");
+    const emptyMessage = cartModal.querySelector(".header__cart-contents");
+    if (emptyMessage) emptyMessage.remove();
+
+    if (quantity === 0) {
+        if (existingItem) existingItem.remove();
+        cartModal.insertAdjacentHTML("beforeend", emptyCartHTML);
+        return;
+    }
+
+
+
+    if (existingItem) {
+        console.log(quantity);
+        if (quantity === 0) {
+            existingItem.remove();
+            return;
+        }
+        const quantityEl = existingItem.querySelector(".cart-quantity");
+        const totalEl = existingItem.querySelector(".cart-total-price");
+
+        quantityEl.textContent = quantity;
+        totalEl.textContent = `$${125 * quantity}.00`;
+        return;
+    }
+
+    const cartItemHTML = `
+        <div class="header__cart-item" id="cart-item">
+            <div class="header__cart-item-details">
+                <div>
+                    <img src="/assets/images/image-product-1-thumbnail.jpg" alt="product"
+                        class="header__cart-item-image">
+                </div>
+                <div>
+                    <p class="header__cart-item-title">Fall Limited Edition Sneakers</p>
+                    <p class="header__cart-item-price">
+                        $125.00 &times;
+                        <span class="cart-quantity" id="cart-quantity">
+                            ${quantity}
+                        </span> =
+                        <strong class="cart-total-price" id="cart-total-price">
+                            $${125 * quantity}.00
+                        </strong>
+                    </p>
+                </div>
+                <div>
+                    <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <defs>
+                            <path
+                                d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
+                                id="a" />
+                        </defs>
+                        <use fill="currentColor" fill-rule="nonzero" xlink:href="#a" />
+                    </svg>
+                </div>
+            </div>
+            <button type="button" class="header__cart-checkout-btn" aria-label="checkout">
+                Checkout
+            </button>
+        </div>
+    `;
+
+
+
+    cartModal.insertAdjacentHTML("beforeend", cartItemHTML);
+}
+
+function updateCartBadge(quantity) {
+    const cartCount = document.getElementById("cart-count");
+    if (quantity >= 0) {
+        cartCount.innerText = quantity;
+        cartCount.style.display = "flex";
+    }
+}
+
+quantityForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(quantityForm));
+    const quantity = parseInt(data.quantity);
+
+    addToCart(quantity);
+    updateCartBadge(quantity);
+    cartModal.classList.add("active");
+});
+
+document.addEventListener("click", (e) => {
+    if (cartModal.classList.contains("active") && !cartModal.contains(e.target)) {
+        cartModal.classList.remove("active");
+    }
+})
+
